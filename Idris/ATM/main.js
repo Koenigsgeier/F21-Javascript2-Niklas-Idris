e75174d268d1491f5b51ec10d67023ed8df561af
Idrisino <57324302+Idrisino@users.noreply.github.com>
2021-06-24 15:23:13 +0200

    ATM

--- Idris/ATM/main.js
+++ Idris/ATM/main.js
@@ -1,19 +1,17 @@
 // Below function executes on submit button.
 function atmPayout() {
-    let money = 180;
+    let money = document.getElementById("money").value;
     let note = [100, 50, 20, 10, 5];
 
     let a = 0;
     let numofno = 0;
-    let output = 0;
     if(money % 5 === 0) {
         for(let i = 0; money > 0; i++) {
             if(money >= note[a]) { // Checks if note fits in money.
                 money = money - note[a]; // Calculates the money left.
                 numofno = numofno + 1;
                 if(money < note[a]) {
-                    output = `${note[a]} X ${numofno}`;
-                    console.log(output);
+                    document.getElementById('result').innerHTML += '<div>' + `<img src="images/${a}.png">` + ' X ' + numofno + '</div><br>';
                     a = a + 1;
                     numofno = 0;
                 }
